5286b0a67c1744dd1922640549c84f877d21e07d
sunil <sunil@hem.com>
2019-04-08 20:56:28 +0530

    Added routes for create and update token

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,12 @@ Rails.application.routes.draw do
     get '/health-checker' => :health_checker
   end
 
+  scope 'api/setup/token', controller: 'api/setup/token', :format => false do
+    get 'create' => :create, via: :POST
+    get 'update' => :update, via: :POST
+  end
+
+
   # Route not found handler. Should be the last entry here
   match '*permalink', to: 'application#not_found', via: :all
 
