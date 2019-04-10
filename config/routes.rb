227bd12f3c56e0e617dcd72ae658117fc9eda355
sunil <sunil@hem.com>
2019-04-10 14:35:34 +0530

    Changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,28 @@
 Rails.application.routes.draw do
 
-  scope '', controller: 'application', :format => false do
+  scope 'demo/api', controller: 'application', :format => false do
     get '/health-checker' => :health_checker
   end
 
-  scope 'setup/token', controller: 'setup/token', :format => false do
-    match 'create' => :create, via: :POST
-    match 'update' => :update, via: :POST
+  scope 'demo/api/setup', controller: 'setup/token', :format => false do
+    match 'create-token' => :create, via: :POST
+    match 'update-token' => :update, via: :POST
   end
 
-  scope 'api/:url_id/:ost_token_id', controller: 'api/authentication', :format => false do
+  scope 'demo/api/:url_id/:ost_token_id', controller: 'api/authentication', :format => false do
     match 'signup' => :signup,  via: :POST
     match 'login' => :login, via: :POST
   end
 
-  scope 'api/:url_id/:ost_token_id/notify', controller: 'api/mobile/notify', :format => false do
+  scope 'demo/api/:url_id/:ost_token_id/notify', controller: 'api/mobile/notify', :format => false do
     match 'user-activate' => :user_activate,  via: :POST
   end
 
-  scope 'api/:url_id/:ost_token_id/users', controller: 'api/token_user', :format => false do
+  scope 'demo/api/:url_id/:ost_token_id/users', controller: 'api/token_user', :format => false do
     match '' => :list, via: :GET
   end
 
-  scope 'api/:url_id/:ost_token_id/users/:token_user_id/devices', controller: 'api/mobile/device', :format => false do
+  scope 'demo/api/:url_id/:ost_token_id/users/:token_user_id/devices', controller: 'api/mobile/device', :format => false do
     match '' => :register, via: :POST
   end
 
