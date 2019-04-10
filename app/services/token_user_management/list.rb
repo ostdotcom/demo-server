9d61b6009e738875d3e95de0626e32ab4b3d7a68
Puneet Khushwani <puneet.khushwani@ost.com>
2019-04-10 15:22:32 +0530

    refactoring in list user service

--- app/services/token_user_management/list.rb
+++ app/services/token_user_management/list.rb
@@ -1,11 +1,14 @@
 module TokenUserManagement
 
-  class List < TokenUserManagement::Base
+  class List
 
     # Get Token Users List Constructor
     #
     def initialize(params)
-      super(params)
+      @token = params[:token]
+      @q = params[:q]
+      @page = params[:page]
+      @limit = 25
     end
 
     # Perform action
@@ -22,9 +25,35 @@ module TokenUserManagement
     # validate params
     #
     def validate_params
-      r = super
+      r = validate_page
       return r unless r[:success]
 
+      r = validate_search_term
+      return r unless r[:success]
+
+      Result.success({})
+    end
+
+    # validate page
+    #
+    def validate_page
+      if @page.present?
+        return Result.error('a_s_tum_l_1', 'INVALID_REQUEST', 'Invalid page') unless Validator.is_numeric?(@page)
+        @page = @page.to_i
+        return Result.error('a_s_tum_l_2', 'INVALID_REQUEST', 'Invalid page') if @page < 1
+      else
+        @page = 1
+      end
+      Result.success({})
+    end
+
+    # validate search term
+    #
+    def validate_search_term
+      if @q.present?
+        return Result.error('a_s_tum_l_3', 'INVALID_REQUEST', 'Invalid page') unless Validator.is_alpha_space?(@q)
+        @q.strip!
+      end
       Result.success({})
     end
 
@@ -42,7 +71,7 @@ module TokenUserManagement
         end
       rescue => e
         Rails.logger.error("get_users exception: #{e.message}")
-        return Result.error('a_s_tum_l_1', 'SERVICE_UNAVAILABLE', 'Service Temporarily Unavailable')
+        return Result.error('a_s_tum_l_4', 'SERVICE_UNAVAILABLE', 'Service Temporarily Unavailable')
       end
 
       Result.success({result_type: 'users', users: users})
