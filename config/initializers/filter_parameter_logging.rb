dd9549503a703a91d939e1a8f85bb3b0ec50e100
Puneet Khushwani <puneet.khushwani@ost.com>
2019-04-23 17:12:07 +0530

    added some params to filter param config

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,4 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password]
+Rails.application.config.filter_parameters += [:password, :cookie_hash, :cookie_string, :secret_key_base]
