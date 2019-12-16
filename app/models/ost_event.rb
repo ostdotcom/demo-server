d24f68310689864a60b62e58afafe45c2685cd89
shlokgilda <shlok@ost.com>
2019-12-16 19:28:27 +0530

    Updated models.

--- app/models/ost_event.rb
+++ app/models/ost_event.rb
@@ -1,2 +1,14 @@
 class OstEvent < ApplicationRecord
+
+  def formated_cache_data
+    {
+      id: id,
+      event_id: event_id,
+      status: status,
+      event_data: event_data,
+      updated_at: updated_at,
+      created_at: created_at
+    }
+  end
+
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,2 +1,14 @@
 class Transaction < ApplicationRecord
+
+  def formated_cache_data
+    {
+      id: id,
+      ost_tx_id: ost_tx_id,
+      status: status,
+      transaction_data: transaction_data,
+      updated_at: updated_at,
+      created_at: created_at
+    }
+  end
+
 end
