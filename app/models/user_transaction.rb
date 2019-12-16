cb01f1d932077cf604ad686830318b349b65c586
shlokgilda <shlok@ost.com>
2019-12-16 19:41:03 +0530

    Updated migration.

--- app/models/user_transaction.rb
+++ app/models/user_transaction.rb
@@ -3,7 +3,7 @@ class UserTransaction < ApplicationRecord
   def formated_cache_data
     {
       id: id,
-      user_id: user_id,
+      token_user_id: token_user_id,
       ost_tx_id: ost_tx_id,
       updated_at: updated_at,
       created_at: created_at
--- db/migrate/20191216121013_create_user_transactions.rb
+++ db/migrate/20191216121013_create_user_transactions.rb
@@ -1,10 +1,10 @@
 class CreateUserTransactions < ActiveRecord::Migration[5.2]
   def change
     create_table :user_transactions do |t|
-      t.column :user_id, :integer, null: false
+      t.column :token_user_id, :integer, null: false
       t.column :ost_tx_id, :string, null: false
       t.timestamps
     end
-    add_index :user_transactions, [:user_id], unique: false, name: 'in_1'
+    add_index :user_transactions, [:token_user_id], unique: false, name: 'in_1'
   end
 end
