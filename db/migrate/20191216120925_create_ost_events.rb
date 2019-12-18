8187204ce23710d5112032c420667124ff7f88fe
shlokgilda <shlok@ost.com>
2019-12-18 20:58:18 +0530

    Updated migrations.

--- db/migrate/20191216120925_create_ost_events.rb
+++ db/migrate/20191216120925_create_ost_events.rb
@@ -1,5 +1,5 @@
 class CreateOstEvents < ActiveRecord::Migration[5.2]
-  def change
+  def up
     create_table :ost_events do |t|
       t.column :event_id, :string, null: false
       t.column :status, :integer, limit:1, null: false
@@ -7,5 +7,9 @@ class CreateOstEvents < ActiveRecord::Migration[5.2]
       t.timestamps
     end
     add_index :ost_events, [:event_id], unique: true, name: 'uk_1'
+  end
+
+  def down
+    drop_table :ost_events
   end
 end
--- db/migrate/20191216121013_create_user_transactions.rb
+++ db/migrate/20191216121013_create_user_transactions.rb
@@ -1,10 +1,14 @@
 class CreateUserTransactions < ActiveRecord::Migration[5.2]
-  def change
+  def up
     create_table :user_transactions do |t|
       t.column :token_user_id, :bigint, limit:20, null: false
       t.column :transaction_id, :bigint, limit:20, null: false
       t.timestamps
     end
     add_index :user_transactions, [:token_user_id], unique: false, name: 'in_1'
+  end
+
+  def down
+    drop_table :user_transactions
   end
 end
