34381adcb22b67cf7e40c108c83a4a277f17cda1
Tejas-Sangani <tejas@ost.com>
2019-12-18 19:13:33 +0530

    subscribe webhooks for existing economy rake task

---
+++ lib/tasks/oneTimer/subscribe_webhooks_for_existing_economy.rb
@@ -0,0 +1,31 @@
+namespace :onetimer do
+
+  # rake RAILS_ENV=staging onetimer:subscribe_webhooks_for_existing_economy
+
+  task :subscribe_webhooks_for_existing_economy => :environment do
+
+    @ost_token_id_to_tokens_hash = {}
+    def perform
+
+      Token.all.each do |token|
+        token_id = token.id
+        ost_token_id = token.ost_token_id
+        api_endpoint_id = token.api_endpoint_id
+        api_key = token.api_key
+        api_secret = CacheManagement::TokenSecureById.new([token_id]).fetch()[token_id][:api_secret]
+
+        @ost_token_id_to_tokens_hash[ost_token_id] = {
+            api_endpoint_id: api_endpoint_id,
+            api_key: api_key,
+            api_secret: api_secret
+        }
+      end
+
+      puts @ost_token_id_to_tokens_hash
+    end
+
+    perform
+
+  end
+
+end
