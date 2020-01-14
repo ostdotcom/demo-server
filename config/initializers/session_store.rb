2fb48c0ccc34d3f4f38756f92b47c7884fe9bf50
santhu210 <santhosh.nitdgp2015@gmail.com>
2020-01-14 16:27:33 +0530

    Cookie secure false

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_ost_demo_session_id', domain: ENV['DEMO_COOKIE_DOMAIN'], http_only: true, secure: !Rails.env.development?, same_site: :strict
+Rails.application.config.session_store :cookie_store, key: '_ost_demo_session_id', domain: ENV['DEMO_COOKIE_DOMAIN'], http_only: true, secure: false, same_site: :strict
